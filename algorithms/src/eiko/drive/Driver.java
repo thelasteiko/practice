17a3cd05190e58267d14cb7d008ffada3eaf78c1
Melinda Robertson <memre911@gmail.com>
2016-08-17 12:12:31 -0700

    First commit.

---
+++ algorithms/src/eiko/drive/Driver.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package eiko.drive;
+
+/**
+ * @author Melinda Robertson
+ * @version 20160817
+ */
+public class Driver {
+
+	/**
+	 * @param args are args.
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ algorithms/src/eiko/drive/Util.java
@@ -0,0 +1,14 @@
+package eiko.drive;
+
+/**
+ * 
+ * @author Melinda Robertson
+ * @version 20160817
+ */
+public class Util {
+	
+	private Util() {}
+	
+	
+
+}
